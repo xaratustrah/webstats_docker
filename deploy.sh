7a40c7579703ffa03b1ed390bd8e0450c453c517
xaratustrah <shahab.sanjari@gmail.com>
2018-10-12 12:23:08 +0200

    create directory if it does not exists

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ docker build -t webstats_docker_nginx:latest -f nginx/Dockerfile nginx/
 docker build -t xaratustrah/webstats_docker:latest -f webstats/Dockerfile webstats/
 
 # Copy the public key to host
+mkdir -p ~/.ssh
 docker run xaratustrah/webstats_docker:latest cat /home/flask/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
 # pass current user and set up the stack
